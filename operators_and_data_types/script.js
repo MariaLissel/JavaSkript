2c3bc9a0ff27ae58e4805e492f35b1f0a08835df
Maria Lissel <lisselmaria@gmail.com>
2024-01-09 10:51:44 +0100

    first commit

---
+++ operators_and_data_types/script.js
@@ -0,0 +1,23 @@
+console.log("M" + "A" + "R" + "I" + "A");
+
+console.log("Hallo");
+
+console.log("That doesn" + "t work");
+
+console.log(50 + 2 === 52);
+console.log(50 + 2 >= 52);
+console.log(50 + 2 <= 52);
+
+console.log("333" < 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+//  Create a console.log that returns true when both division results are equal to 5
+console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5
+
+console.log(25 / 5 === 5 && 25 / 5 === 5);
