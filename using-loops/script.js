4ac8a8c4bf76ba6dc6aacb53ef159aaeac211471
Maria Lissel <lisselmaria@gmail.com>
2024-01-12 16:55:49 +0100

    warum funktioniert der Kram immer noch nicht?

--- using-loops/script.js
+++ using-loops/script.js
@@ -1,13 +1,7 @@
 // TODO: Implement the oddNumbers function
 
-/*
-function oddNumbers(a, b) {
-  for (let i = 0; i > a && i < b; i++) {
-
-
 /* Iterate alle Nummern zwischen der erstsen und der zweiten nummer 
 Bsp. a = 0 und b = 4 Result ist 1,2,3
-
 Speichere alle ungerade Zahlen als string und gib sie von Kommas getrennt aus und return den string
 Bsp. 1,3
 */
@@ -16,14 +10,17 @@ function oddNumbers(a, b) {
   if (a < 0 || b < 0) {
     return "must be non-negative.";
   }
-  let oddNumberStore = ""; // ein Container/Speicher für den String als Leeraum
+  let result = ""; // ein Container/Speicher für den String als Leeraum
 
-  for (let inumber = a; inumber < b; inumber++) {
+  for (let inumber = a; inumber <= b; inumber++) {
     if (inumber % 2 !== 0) {
-      oddNumberStore += inumber + ",";
+      if (result.length > 0) {
+        result = result += inumber + ",";
+      }
+      result = result + inumber;
     }
   }
-  return oddNumberStore;
+  return result;
 }
 
 /* 
@@ -32,7 +29,7 @@ function oddNumbers(a, b) {
 3. inumber bekommt in jeder iteration einen Wert +
 */
 
-console.log(oddNumbers(0, -4));
+console.log(oddNumbers(0, 4));
 // result should be: 1,3
 
 console.log(oddNumbers(10, 33));
